2e5d998e3daeb28a8fa20ada659a46490ed6692e
FlippingBinary <35066367+FlippingBinary@users.noreply.github.com>
2021-03-18 13:29:54 +0000

    ignore some flake8 errors

--- wqxlib/__init__.py
+++ wqxlib/__init__.py
@@ -1,6 +1,6 @@
-from .Document import Document
-from .Header import Header
-from .ImportConfiguration import ImportConfiguration
-from .Payload import Payload
-from .Submission import Submission
-from .wqx_v3_0 import *
+from .Document import Document # noqc F401
+from .Header import Header # noqc F401
+from .ImportConfiguration import ImportConfiguration # noqc F401
+from .Payload import Payload # noqc F401
+from .Submission import Submission # noqc F401
+from .wqx_v3_0 import * # noqc F401,F403
