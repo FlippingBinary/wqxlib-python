9ac20528043ee983c64bce5577c49882c8f001cf
FlippingBinary <35066367+FlippingBinary@users.noreply.github.com>
2021-06-08 16:42:29 +0000

    Publish new monitoring location hook

--- version.py
+++ version.py
@@ -3,8 +3,8 @@ wqx_v = "3.0"
 # Major version: breaking changes
 major_v = "0"
 # Minor version: new features
-minor_v = "4"
+minor_v = "5"
 # Patch version: bug fixes / no interface changes
-patch_v = "2"
+patch_v = "0"
 
 __version__ = f"{wqx_v}.{major_v}.{minor_v}.{patch_v}"
